d76722e1c894cfdd133358e9190759a68c1a1e86
mahmad-LU <mahmad@lakeheadu.ca>
2020-11-19 16:37:05 -0500

    new

--- main.go
+++ main.go
@@ -44,7 +44,7 @@ func main() {
 func hello(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Serving request: %s", r.URL.Path)
 	host, _ := os.Hostname()
-	fmt.Fprintf(w, "Hello, world! fixed code 2ice\n")
+	fmt.Fprintf(w, "Hello, world! fixed code 3rdce\n")
 	fmt.Fprintf(w, "Version: 1.0.0\n")
 	fmt.Fprintf(w, "Hostname: %s\n", host)
 }
